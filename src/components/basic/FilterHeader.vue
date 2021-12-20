7a70cf27f9e7eeb69fd198723684b2456989a008
bertyn99 <bb@gmail.com>
2021-12-20 22:09:08 +0100

    les bouton du header mis dans leur compôsant

---
+++ src/components/basic/FilterHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search-filter">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="icon search-icon"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
+      />
+    </svg>
+    <div></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.search-filter {
+}
+
+.search-icon:hover {
+  opacity: 1;
+}
+</style>
---
+++ src/components/basic/SearchHeader.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="search-box">
+    <input type="text" />
+    <button class="search-button" @click="alert()">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="icon"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss">
+.search-box {
+  padding: 5px;
+  height: var(--spacer);
+
+  border-radius: var(--space-md);
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-right: var(--spacer);
+  padding: 10px 4px;
+  &:hover {
+    background: lighten($secondary, 20%);
+    & > input {
+      width: 240px;
+      padding: 0 6px;
+    }
+    & > .search-button {
+      background: lighten($primary, 20%);
+      color: var(--secondary-color);
+
+      & .icon {
+        color: var(--text-color);
+        opacity: 1;
+      }
+    }
+  }
+
+  & input {
+    border-radius: 4rem;
+    float: left;
+    outline: none;
+    border: none;
+    padding: 0;
+    font-size: var(-text-size);
+    background: transparent;
+    transition: 0.4s;
+    line-height: 30px;
+    width: 0px;
+  }
+}
+.search-button {
+  float: right;
+  border-radius: var(--spacer);
+  border: none;
+  padding: 2px 4px;
+  transition: all 0.4s;
+  height: var(--space-md);
+  width: var(--space-md);
+  background: transparent;
+  display: flex;
+  justify-content: center;
+}
+</style>
