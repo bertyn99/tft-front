25380998fcfd4a72e6bf08e1a22ccb3bcec1c5d6
bertyn99 <bb@gmail.com>
2021-12-20 22:08:31 +0100

    add container class

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,18 @@ export default {
 html,
 body {
   margin: 0px !important;
-  padding: 0px !important;
+  padding: 0 0 4rem 0 !important;
   overflow: hidden;
+  background: var(--primary-color);
+}
+.container {
+  padding: 2rem 0.5rem 16rem 0.5rem;
+  margin: 0 auto;
+  @include bq(medium) {
+    max-width: map-get($breakpoints, medium);
+  }
+  @include bq(large) {
+    max-width: map-get($breakpoints, large);
+  }
 }
 </style>
