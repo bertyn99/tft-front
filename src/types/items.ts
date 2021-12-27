27edd7000bbea0aaffc51a34242706208bcdd872
bertyn99 <bb@gmail.com>
2021-12-27 15:55:35 +0100

    ajout type items

---
+++ src/types/items.ts
@@ -0,0 +1,25 @@
+import { ModificationNote } from "./common";
+export interface Effects {
+  health?: number;
+  armor?: number;
+  as?: number;
+  ap?: number;
+  ad?: number;
+  damage?: number;
+  bonusAd?: number;
+  magicResist?: number;
+  CritChance?: number;
+  mana?: number;
+  bonusAs?: number;
+  magicReduction?: number;
+}
+export interface Item {
+  name: string;
+  desc: string;
+  effect: Effects;
+  icon: string;
+  unique: boolean;
+  from: number[];
+
+  modification_notes: ModificationNote[];
+}
